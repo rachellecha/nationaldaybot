a211dd6b18b4431e4bc45c8fa285bb4675eb2f71
rachellecha <rachelleycha@gmail.com>
2024-01-16 21:24:25 -0500

    fix

--- modules.py
+++ modules.py
@@ -24,19 +24,19 @@ def summ(convo):
 #webscrape to get all the silly holidays per day!
 #returns all the holidays as a list
 def dayGenerator():
-    URL = 'https://www.holidaycalendar.io/what-holiday-is-today'
+    URL = 'https://www.holidaycalendar.io/what-is-today'
     page = requests.get(URL)
 
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    result = soup.find_all("div", class_="collection-item-2 w-dyn-item")
+    result = soup.find_all("h3", class_="card-link-title---hover-secondary-1 display-4 mg-bottom-2px")
 
     days = []
 
     for a in result:
-        name = a.find("h3", class_="card-link-title---hover-secondary-1 display-4 mg-bottom-2px")
-        if "Day" in name.text:
-            days.append(name.text)
+        name = a.get_text()
+        if "Day" in name:
+            days.append(name)
     return days
 
 #get the keywords of the holidays
